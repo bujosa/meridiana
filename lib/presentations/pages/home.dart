cb820351f17c5d4fa68e4e265183a21e027e9ab8
bujosa <davidbujosa@gmail.com>
2023-01-26 23:36:46 -0400

    feat(provider): create subcription provider

---
+++ lib/presentations/pages/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:meridiana/providers/subscription.dart';
+import 'package:provider/provider.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final subscriptionProvider = Provider.of<SubscriptionProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Subscribe ${subscriptionProvider.isSubscribed}',
+              style: const TextStyle(fontSize: 50),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            {_incrementCounter(), subscriptionProvider.switchProvider()},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
