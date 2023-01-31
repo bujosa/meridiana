45a7051be0d1441454134a2035e2a87ad6830b83
bujosa <davidbujosa@gmail.com>
2023-01-31 00:18:44 -0400

    feat: add title in home page

--- lib/presentations/pages/home.dart
+++ lib/presentations/pages/home.dart
@@ -12,28 +12,84 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // Use colors from the theme
         backgroundColor: Theme.of(context).colorScheme.background,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'Home Page',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 80),
+              child: Text.rich(
+                TextSpan(
+                  text: 'Hello, ',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'John',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.secondary,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 60),
+            Center(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.euro,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  Text(
+                    '1000',
+                    style: TextStyle(
+                        color: Colors.white,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 300.0,
+                            color: Colors.white,
+                            offset: Offset(0, 65),
+                          ),
+                        ],
+                        fontSize: 80,
+                        overflow: TextOverflow.clip),
+                  ),
+                ],
+              ),
+            ),
+            const Center(
+              child: Text('Monthly payments',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  )),
+            ),
+            const SizedBox(height: 70),
+            Center(
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Center(
+                  child: Text('Pie Chart'),
+                ),
+              ),
+            ),
+          ],
         ),
         bottomNavigationBar: const BottomNavigationBarWidget(index: 0));
   }
 }
-
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
