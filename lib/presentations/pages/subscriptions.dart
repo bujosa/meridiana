d2bc00a9d2ff0590bb4dcc57a00f88d81036e1ef
bujosa <davidbujosa@gmail.com>
2023-01-31 02:41:04 -0400

    feat: add example related with the subscription widget

--- lib/presentations/pages/subscriptions.dart
+++ lib/presentations/pages/subscriptions.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:meridiana/presentations/models/subscription.dart';
 import '../widgets/bottom_navigation_bar.dart';
 
 class SubscriptionsPage extends StatefulWidget {
@@ -9,23 +11,128 @@ class SubscriptionsPage extends StatefulWidget {
 }
 
 class _SubscriptionsPageState extends State<SubscriptionsPage> {
+  List<Subscription> subscriptions = [
+    Subscription(
+        name: 'Spotify',
+        price: 5.99,
+        category: 1,
+        image:
+            'https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png'),
+    Subscription(
+        name: 'Netflix',
+        price: 9.99,
+        category: 1,
+        image:
+            'https://www.freepnglogos.com/uploads/netflix-logo-circle-png-5.png'),
+    Subscription(
+        name: 'Disney+',
+        price: 7.99,
+        category: 1,
+        image:
+            'https://seeklogo.com/images/D/disney-logo-575AED0F1D-seeklogo.com.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // Use colors from the theme
         backgroundColor: Theme.of(context).colorScheme.background,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'Subscriptions Page',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40,
-                ),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Theme.of(context).colorScheme.background,
+          elevation: 0,
+          title: Text(
+            'Subscriptions',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 30,
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: IconButton(
+                icon: Icon(Icons.add_box,
+                    size: 35, color: Theme.of(context).colorScheme.primary),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/add_subscription');
+                },
               ),
-            ],
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: ListView.builder(
+            itemCount: subscriptions.length,
+            itemBuilder: (context, index) {
+              final subscription = subscriptions[index];
+              return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+                  child: Card(
+                    child: ListTile(
+                      leading: CachedNetworkImage(
+                        imageUrl: subscription.image,
+                        imageBuilder: (context, imageProvider) => Container(
+                          width: 40.0,
+                          height: 40.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: imageProvider, fit: BoxFit.cover),
+                          ),
+                        ),
+                        progressIndicatorBuilder:
+                            (context, url, downloadProgress) =>
+                                CircularProgressIndicator(
+                                    value: downloadProgress.progress),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      ),
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(subscription.name,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              )),
+                          Text('${subscription.price} / mo',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                              )),
+                        ],
+                      ),
+                      subtitle: const Text('Streaming'),
+                      onLongPress: () => {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text('Delete'),
+                                content: const Text('Are you sure?'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () => {
+                                      setState(() {
+                                        subscriptions.removeAt(index);
+                                      }),
+                                      Navigator.pop(context)
+                                    },
+                                    child: const Text('Delete'),
+                                  ),
+                                ],
+                              );
+                            })
+                      },
+                    ),
+                  ));
+            },
           ),
         ),
         bottomNavigationBar: const BottomNavigationBarWidget(index: 1));
