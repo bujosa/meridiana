1ec325981fc841e0d2b43b4b4ccdbe7521734d57
bujosa <davidbujosa@gmail.com>
2023-01-31 18:29:48 -0400

    feat: create category map

---
+++ lib/shared/utils/category_map.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+const Map<int, Icon> currencyToIcon = {
+  0: Icon(
+    Icons.attach_money,
+    color: Colors.amber,
+  ),
+  1: Icon(
+    Icons.euro,
+    color: Colors.amber,
+  ),
+  2: Icon(
+    Icons.attach_money_outlined,
+    color: Colors.amber,
+  ),
+};
+
+const Map<int, Icon> currencyToIconHomePage = {
+  0: Icon(
+    Icons.attach_money,
+    color: Colors.white,
+    size: 40,
+  ),
+  1: Icon(
+    Icons.euro,
+    color: Colors.white,
+    size: 40,
+  ),
+  2: Icon(
+    Icons.attach_money_outlined,
+    color: Colors.white,
+    size: 40,
+  ),
+};
+
+
+
+const Map<int, String> categoryToEmoji = {
+  0: '🏥',
+  1: '👨‍👩‍👧‍👦',
+  2: '📚',
+  3: '👨‍💻'
+};
---
+++ lib/shared/utils/constants.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+List dropdownCurrencyList = [
+  {
+    'label': 'Dollar',
+    'value': 0,
+    'icon': const Icon(
+      Icons.attach_money,
+      color: Colors.amber,
+    )
+  },
+  {
+    'label': 'Euro',
+    'value': 1,
+    'icon': const Icon(
+      Icons.euro,
+      color: Colors.amber,
+    )
+  },
+  {
+    'label': 'Peso Dominicano',
+    'value': 2,
+    'icon': const Icon(
+      Icons.attach_money_outlined,
+      color: Colors.amber,
+    )
+  },
+];
+
+List dropdownCategoryList = [
+  {
+    'label': 'Streaming',
+    'value': 1,
+  },
+  {
+    'label': 'Lighting',
+    'value': 2,
+  },
+  {
+    'label': 'Gaming',
+    'value': 3,
+  },
+];
